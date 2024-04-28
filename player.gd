cbc8ec96225c46dcfe1ea608a81b42f87cc1a55c
Louise Malvin Tanaka <louise.malvin12@gmail.com>
2024-04-28 22:51:40 +0700

    feat: add dodge mechanics to player

--- player.gd
+++ player.gd
@@ -1,7 +1,11 @@
 extends Area2D
-
+#Add
+@export var dodge_cooldown = 0.5
 @export var speed = 400 # How fast the player will move (pixels/sec).
 @export var dodge_multiplier = 2
+@export var dodge_duration = 0.2
+var dodge_timer = 0.0
+var cooldown_timer = 0.0
 var screen_size # Size
 
 func _ready():
@@ -17,11 +21,17 @@ func _process(delta):
 		velocity.x += 1
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
+	if Input.is_action_pressed("dodge") and is_dodge_ready():
+		dodge_timer += dodge_duration
+		cooldown_timer += dodge_cooldown
 		
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		if Input.is_action_pressed("dodge"):
+		if dodge_timer > 0.0:
 			velocity *= dodge_multiplier
+			dodge_timer -= delta
+		
+			
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
@@ -36,4 +46,10 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
+	
+	if cooldown_timer > 0.0:
+		cooldown_timer -= delta
+	
+func is_dodge_ready():
+	return cooldown_timer <= 0.0
 	
